c1350070e02f1445d1abd3d9f14b2bc6382915f9
senchiiry <48319049+senchiiry@users.noreply.github.com>
2019-03-13 20:42:49 +0800

    选项非空

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -9,6 +9,14 @@ Page({
   * 页面的初始数据
   */
   data: {
+    name: "",
+    phone: '',
+    idcard: "",
+    type: "",
+    defaultValue: "",
+    description: "",
+    tip: "",
+    require: ""
 
 
   },
@@ -17,20 +25,40 @@ Page({
   //确认按钮把数据上传后台
   back_houtai: function (e) {
     var that = this;
-    var total_score = e.detail.value.name;
-    var major_score = e.detail.value.idcard;
-    var enroll_date = e.detail.value.phone;
+    var name = e.detail.value.name;
+    var idcard = e.detail.value.idcard;
+    var phone = e.detail.value.phone;
     var self_intro = e.detail.value.self_intro;
     console.log(name)
     console.log(idcard)
     console.log(phone)
     console.log(self_intro)
+    if (name == "" || phone == "" || idcard == "") {
+      wx.showModal({
+        title: '提示',
+        content: '请输入正确完整的信息！',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      })
+    }
+    else {
+      console.log(e.detail.value)
+    }
+
+
+
   },
 
   /**
   * 生命周期函数--监听页面加载
   */
+
   onLoad: function (options) {
   },
+
+
 
 })
