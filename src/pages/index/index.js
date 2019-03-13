51e4e7d136b3066dbe055952f4017b6771d50fb2
senchiiry <48319049+senchiiry@users.noreply.github.com>
2019-03-13 16:21:56 +0800

    signuptext

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,13 +3,34 @@
 const app = getApp()
 
 Page({
+
+
+  /**
+  * 页面的初始数据
+  */
   data: {
-    skip: false
+
+
   },
-  change() {
-    this.setData({
-      skip: false
-    })
-  }
+
+
+  //确认按钮把数据上传后台
+  back_houtai: function (e) {
+    var that = this;
+    var total_score = e.detail.value.name;
+    var major_score = e.detail.value.idcard;
+    var enroll_date = e.detail.value.phone;
+    var self_intro = e.detail.value.self_intro;
+    console.log(name)
+    console.log(idcard)
+    console.log(phone)
+    console.log(self_intro)
+  },
+
+  /**
+  * 生命周期函数--监听页面加载
+  */
+  onLoad: function (options) {
+  },
 
 })
